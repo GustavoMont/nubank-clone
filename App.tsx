c5c882a39258bdae4fe4aa9eba05c046d65bc42d
GustavoMont <lgmluisgm@gmail.com>
2022-10-18 23:25:53 -0300

    :tada: start app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import colors from "./src/styles/colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={colors.mainColor}
+        />
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.mainColor,
   },
 });
---
+++ src/styles/colors.ts
@@ -0,0 +1,5 @@
+export default {
+  lightMainColor: "#BA4DE3",
+  mainColor: "#8A05BE",
+  white: "#F5F5F5",
+};
